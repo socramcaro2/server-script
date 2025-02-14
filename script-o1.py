f88f86100c4a9bd059566f011a6df17d12efe6bf
Socramcaro2 <socramcaro@gmail.com>
2025-02-14 12:02:49 +0100

    Unfinished script, still making traefik setup

--- script-o1.py
+++ script-o1.py
@@ -1,4 +1,7 @@
+
+
 import os
+import sys
 
 ################################################################################
 # Esta funcion solo cambiaria el menu sin afectar al funcionamiento del script #
@@ -47,20 +50,17 @@ def script():
         
     elif opcion == "3":
       portainer()
-      regreso_al_menu()
-
+      regreso_al_menu() 
     elif opcion == "4":
       traefik()
-      regreso_al_menu()
-
+      regreso_al_menu() 
     elif opcion == "5":
       docker()
       traefik()
       regreso_al_menu()
       
     elif opcion == "0":
-      print("\n  Bye!")
-
+      print("\n  Bye!") 
     else:
       correccion = input('\n  Introduce un input valido: ')
       opcion = str(correccion)
@@ -87,12 +87,147 @@ def docker():
 def portainer():
   os.system()
   
-def traefik():
-  os.system('sudo su')
+
   
+  
+def traefik():
+  # el script se hizo en mente con traefik 3.0 en mente
+  
+  # variables para personalizar la instalacion
+  Cloudfare_Api = input('Cual es tu Api de cloudfare: ')
+  Tu_dominio = input("cual es tu Dominio? debes ser el dueño: ") 
+  Tu_correo = input('ingresa tu email de cloudfare: ')
+  Usuario_panel = input('ingresa usuario para el panel de traefik: ')
+  Contraseña_panel = input('ingresa la contraseña para el panel de traefik: ')
+  Confrimar_contraseña = input('vuelve a introducir la contraseña: ')
+  
+  # verificar contraseñas correctas
+  
+  i=0
+  
+  while i == 0:
+    if Contraseña_panel == Confrimar_contraseña:
+      i +=1
+    else:
+      print('\n \n Introduce correctamente las contraseñas \n \n')
+      Contraseña_panel = input('ingresa la contraseña para el panel de traefik: ')
+      Confrimar_contraseña = input('vuelve a introducir la contraseña: ')
+      
+
+  # contenido de los archivos que se van a crear
+  Docker_Compose = f"""version: "3.8"
+
+services:
+  traefik:
+    image: traefik
+    container_name: traefik
+    restart: unless-stopped
+    security_opt:
+      - no-new-privileges:true
+    networks:
+      - proxy
+    ports:
+      - 80:80
+      - 443:443
+      # - 443:443/tcp # Uncomment if you want HTTP3
+      # - 443:443/udp # Uncomment if you want HTTP3
+    environment:
+      CF_DNS_API_TOKEN_FILE: /run/secrets/cf_api_token # note using _FILE for docker secrets
+      # CF_DNS_API_TOKEN: ${{CF_DNS_API_TOKEN}} # if using .env
+      TRAEFIK_DASHBOARD_CREDENTIALS: ${{TRAEFIK_DASHBOARD_CREDENTIALS}}
+    secrets:
+      - cf_api_token
+    env_file: .env # use .env
+    volumes:
+      - /etc/localtime:/etc/localtime:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      - ./data/traefik.yml:/traefik.yml:ro
+      - ./data/acme.json:/acme.json
+      - ./data/config.yml:/config.yml:ro
+    labels:
+      - "traefik.enable=true"
+      - "traefik.http.routers.traefik.entrypoints=http"
+      - "traefik.http.routers.traefik.rule=Host(`traefik-dashboard.local.{Tu_dominio}`)"
+      - "traefik.http.middlewares.traefik-auth.basicauth.users=${{TRAEFIK_DASHBOARD_CREDENTIALS}}"
+      - "traefik.http.middlewares.traefik-https-redirect.redirectscheme.scheme=https"
+      - "traefik.http.middlewares.sslheader.headers.customrequestheaders.X-Forwarded-Proto=https"
+      - "traefik.http.routers.traefik.middlewares=traefik-https-redirect"
+      - "traefik.http.routers.traefik-secure.entrypoints=https"
+      - "traefik.http.routers.traefik-secure.rule=Host(`traefik-dashboard.local.{Tu_dominio}`)"
+      - "traefik.http.routers.traefik-secure.middlewares=traefik-auth"
+      - "traefik.http.routers.traefik-secure.tls=true"
+      - "traefik.http.routers.traefik-secure.tls.certresolver=cloudflare"
+      - "traefik.http.routers.traefik-secure.tls.domains[0].main=local.{Tu_dominio}"
+      - "traefik.http.routers.traefik-secure.tls.domains[0].sans=*.local.{Tu_dominio}"
+      - "traefik.http.routers.traefik-secure.service=api@internal"
+
+secrets:
+  cf_api_token:
+    file: ./cf_api_token.txt
+
+networks:
+  proxy:
+    external: true"""
+     
+#######################################################################################################################################################
+####################################################################################################################################################### si no no los diferencio
+#######################################################################################################################################################
+  
+  traefik_yml = f"""api:
+  dashboard: true
+  debug: true
+entryPoints:
+  http:
+    address: ":80"
+    http:
+      redirections:
+        entryPoint:
+          to: https
+          scheme: https
+  https:
+    address: ":443"
+serversTransport:
+  insecureSkipVerify: true
+providers:
+  docker:
+    endpoint: "unix:///var/run/docker.sock"
+    exposedByDefault: false
+  # file:
+  #   filename: /config.yml
+certificatesResolvers:
+  cloudflare:
+    acme:
+      email: {Tu_correo}
+      storage: acme.json
+      # caServer: https://acme-v02.api.letsencrypt.org/directory # prod (default)    # esta comentado mientras pruebo el script para que no me baneen, NO OLVIDAR COMENTAR
+      caServer: https://acme-staging-v02.api.letsencrypt.org/directory # staging
+      dnsChallenge:
+        provider: cloudflare
+        #disablePropagationCheck: true # uncomment this if you have issues pulling certificates through cloudflare, By setting this flag to true disables the need to wait for the propagation of the TXT record to all authoritative name servers.
+        #delayBeforeCheck: 60s # uncomment along with disablePropagationCheck if needed to ensure the TXT record is ready before verification is attempted 
+        resolvers:
+          - "1.1.1.1:53"
+          - "1.0.0.1:53" """
+    
+  #comandos que se ejecutaran
+  os.system('sudo su')
   os.system('mkdir docker_volumes && cd docker_volumes')
   os.system('mkdir traefik && cd traefik')
-  os.system('')
+  os.system('touch docker-compose.yaml')
+  with open('docker-compose.yaml', "w", encoding="utf-8") as Docker_archivo: # escribir sobre el archivo
+    Docker_archivo.write(Docker_Compose)
+  os.system('touch cf_api_token.txt')
+  with open('cf_api_token.txt', "w", encoding='utf-8') as TOKEN:
+    TOKEN.write(Cloudfare_Api)
+  os.system('mkdir data && cd data')
+  os.system('touch acme.json && chmod 600 acme.json')
+  os.system('touch traefik.yml')
+  with open('traefik.yml', "w", encoding="utf-8") as traefikyml:
+    traefikyml.write(traefik_yml)
+  os.system('docker network create proxy')
+  os.system(f'echo $(htpasswd -nbB {Usuario_panel} {Contraseña_panel}) | sed -e s/\\$/\\$\\$/g >> .env')
+  
+  
   
   os.system('exit')
   
@@ -103,6 +238,17 @@ def regreso_al_menu():
 #############
 # ejecucion #
 #############
+os.system('sudo apt update && sudo apt upgrade -y')
+
+
+# COMENTADO PARA PROBAR EN WINDOWS, DESCOMENTAR CUANDO SE FINALICE
+# if os.geteuid() != 0:
+#   print('ejecuta con permisos sudo por favor')
+# else:
+#   os.system('sudo apt update && sudo apt upgrade -y')
+#   INPUT()
+#   script()
+
 
 INPUT()
 script()
